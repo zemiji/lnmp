f6c274cff83a4feef12b6c144bd32cdc91ea25ee
zemiji <zengjia@cengjiadeMacBook-Pro.local>
2019-11-05 11:40:52 +0800

    解决数据库连接

--- www/index.php
+++ www/index.php
@@ -1,2 +1,17 @@
 <?php
+$servername = "mysql";
+$username = "root";
+$password = "123456";
+ 
+echo "数据库";
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功";
+echo "<br/>";
+
 phpinfo();
